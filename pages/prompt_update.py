b1ad1a87a2df6b6b8fae894dc3074dd1032c55c4
twosom <two_somang@icloud.com>
2025-05-18 13:43:04 +0900

    feat: add GCS prompt reading and display existing prompt in form

--- pages/prompt_update.py
+++ pages/prompt_update.py
@@ -7,13 +7,15 @@ st.set_page_config(page_title="Update Prompts", page_icon="✏️")
 # header
 st.title("Update Prompts")
 
-st.write("Use the form below to update the prompts to pass it as side input to pipeline")
+st.write(
+    "Use the form below to update the prompts to pass it as side input to pipeline"
+)
+# Initialize GCS client
+client = storage.Client()
 
 
 def upload_prompt_to_gcs(bucket_name, file_name, content):
     try:
-        # Initialize GCS client
-        client = storage.Client()
         bucket = client.bucket(bucket_name)
         blob = bucket.blob(file_name)
         blob.upload_from_string(content)
@@ -23,24 +25,43 @@ def upload_prompt_to_gcs(bucket_name, file_name, content):
         return False
 
 
+def read_prompt_from_gcs(bucket_name, file_name):
+    try:
+        bucket = client.bucket(bucket_name)
+        blob = bucket.blob(file_name)
+
+        if blob.exists():
+            content = blob.download_as_text()
+            return content
+        else:
+            return ""
+    except Exception as e:
+        st.error(f"An error occurred while reading from GCS: {e}")
+        return ""
+
+
+# GCS configuration
+GCS_BUCKET_NAME = "pt-prompt-templates"
+GCS_FILE_NAME = "new_prompt.txt"
+
+# Read existing prompt from GCS
+existing_prompt = read_prompt_from_gcs(GCS_BUCKET_NAME, GCS_FILE_NAME)
+print(existing_prompt)
+
 # Form for updating prompts
-with st.form(key='prompt_update_form'):
-    # Input for new prompt
-    new_prompt = st.text_area("Enter the new prompt:", height=150)
+with st.form(key="prompt_update_form"):
+    # Input for new prompt, pre-filled with existing prompt
+    new_prompt = st.text_area(
+        "Enter the new prompt:", value=existing_prompt, height=150
+    )
 
     # Submit button
     submit_button = st.form_submit_button("Update Prompt")
 
     if submit_button:
         if new_prompt:
-            # GCS configuration
-
-            GCS_BUCKET_NAME = "pt-prompt-templates"
-            GCS_FILE_NAME = "new_prompt.txt"
-
             # Upload prompt to GCS
-            success = upload_prompt_to_gcs(
-                GCS_BUCKET_NAME, GCS_FILE_NAME, new_prompt)
+            success = upload_prompt_to_gcs(GCS_BUCKET_NAME, GCS_FILE_NAME, new_prompt)
             if success:
                 st.success("Prompt updated and uploaded to GCS successfully!")
         else:
